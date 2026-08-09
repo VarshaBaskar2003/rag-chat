d90b70343eac0f2213b999a2b5f1f4856617aa7e
Varsha B <varshab0109@gmail.com>
2026-08-09 15:25:01 +0530

    Fix App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Sidebar from "./components/Sidebar";
+import Sidebar from "./components/SideBar";
 import Header from "./components/Header";
 import ChatWindow from "./components/ChatWindow";
 import ChatInput from "./components/ChatInput";
